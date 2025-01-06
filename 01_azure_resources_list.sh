007a5a0942ce3fcda2aa56ddc0354d6096b6e708
86nsarada <89052036+86nsarada@users.noreply.github.com>
2025-01-06 15:04:18 -0500

    adding azure resources list program sudo code

---
+++ 01_azure_resources_list.sh
@@ -0,0 +1,14 @@
+# ######### List all the azure resources we're using in our organization  ###########################
+# 1. This shell script will going to list all resources in azure account
+# 2. Author : Sarada
+# 3. Version: V0.0.1
+# 4. Date etc.,
+# 5. Folloing are the supported azure services by this script
+# 6. VM , Vnet, Subnets, Storage accounts, blobs, Fileshares, IAM, AKS, etc.,
+#
+#
+# 7. Usage: run this command "./01_azure_resource_list.sh <region_name> <service_name>"
+#######################################################################################################
+
+if()
+
